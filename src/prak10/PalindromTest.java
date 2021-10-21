e48560f047cd45beeb293daea0b6530b3edf1626
Nik_HOME <eulanoff.nik-now@yandex.ru>
2021-10-21 11:46:20 +0300

    prak11 and prak13

--- src/prak10/PalindromTest.java
+++ src/prak10/PalindromTest.java
@@ -2,6 +2,6 @@ package prak10;
 public class PalindromTest {
     public static void main(String[] args){
         Palindrom d1 = new Palindrom();
-        System.out.println(d1.isPalindromeRec("zakab"));
+        System.out.println(d1.isPalindromeRec("zakaz"));
     }
 }
--- src/prak10/Razvorot_chisla.java
+++ src/prak10/Razvorot_chisla.java
@@ -5,6 +5,6 @@ public class Razvorot_chisla {
             return (n==0) ? i : recursion( n/10, i*10 + n%10 );
         }
         public static void main(String[] args) {
-            System.out.println(recursion(158, 0));
+            System.out.println(recursion(123, 0));
         }
 }
